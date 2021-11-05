dae4102b239ac593f3b246f490058c924633b940
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-11-05 23:05:44 +0800

    edit userSeeder

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -19,3 +19,25 @@ const SEED_USERS = [{
     password: '0000'
   }
 ]
+
+db.once('open', () => {
+  Promise
+    .all(Array.from(
+      SEED_USERS,
+      SEE_USER => {
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(SEED_USER.password, salt))
+          .then(hash => User.create({
+            name: SEED_USER.name,
+            email: SEED_USER.email,
+            password: hash
+          }))
+      }
+    ))
+    .then(() => {
+      consolo.log('SEED_USERS is done.')
+      process.exit()
+    })
+    .catch(err => console.error(err))
+})
