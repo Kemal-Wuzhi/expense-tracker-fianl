2cca5426a2fbf40822a693d13d9c70526d5122d5
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-11-05 23:08:16 +0800

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 
-router.use('/', home)
-router.use('/records', records)
+const users = require('./modules/users')
+const auth = require('./modules/auth')
+
+const {
+  authenticator
+} = require('../middleware/auth')
+
+router.use('/', authenticator, home)
+router.use('/records', authenticator, records)
+router.use('/users', users)
+router.use('/auth', auth)
 
 module.exports = router
