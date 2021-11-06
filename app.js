97d03f7e597d9abc7e646df3ec8962013c832d3f
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-11-06 22:56:38 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const ifEqual = require('./tools/handlebarshelpers')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const routes = require('./routes')
-const session = require('express-session')
-const flash = require('connect-flash')
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT
 
-
-
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
   helpers: {
-    ifEqual
+    isEqual: (a, b) => a === b
   }
 }))
 app.set('view engine', 'hbs')
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
-  })
-)
-
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({
   extended: true
 }))
 app.use(methodOverride('_method'))
-
-app.use(flash())
 usePassport(app)
-//check again
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
@@ -53,5 +46,5 @@ app.use((req, res, next) => {
 app.use(routes)
 
 app.listen(PORT, () => {
-  console.log(`This is listening on http://localhost:${PORT}`)
+  console.log(`Server is listening on http://localhost:${PORT}`)
 })
